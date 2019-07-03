6e893778c90f8c8d1496245bc914dd4770014a20
nagaohiroki <testplayers0226@gmail.com>
2019-07-03 13:57:55 +0900

    add game

---
+++ UnityProject/Assets/Scripts/Dog.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class Dog : MonoBehaviour
+{
+	[SerializeField]
+	GameObject mMonster = null;
+	[SerializeField]
+	Rigidbody mRigidbody = null;
+	void Update()
+	{
+		var vec = transform.position - mMonster.transform.position;
+		if(vec.magnitude < 3.0f)
+		{
+			// mRigidbody.AddForce(vec.normalized, ForceMode.VelocityChange);
+			transform.position += vec * 0.15f;
+		}
+	}
+}
---
+++ UnityProject/Assets/Scripts/Monster.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+public class Monster : MonoBehaviour
+{
+	[SerializeField]
+	Rigidbody mRigidbody = null;
+	void Update()
+	{
+		var vec = Vector3.zero;
+		vec.x = Input.GetAxis("Horizontal");
+		vec.z = Input.GetAxis("Vertical");
+		transform.position += vec.normalized * 0.1f;
+	//	if(mRigidbody != null)
+	//	{
+	//		mRigidbody.AddForce(vec, ForceMode.VelocityChange);
+	//	}
+	}
+}
