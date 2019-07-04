1aaad18473910170a6f04d2a8a8aae9c5b859727
nagaohiroki <testplayers0226@gmail.com>
2019-07-04 13:59:14 +0900

    add new game

---
+++ UnityProject/Assets/Scripts/Cat.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+public class Cat : MonoBehaviour
+{
+	[SerializeField]
+	GameObject mCameraHandle = null;
+	[SerializeField]
+	Rigidbody mRigid = null;
+	void Update()
+	{
+		if(mCameraHandle != null)
+		{
+			mCameraHandle.transform.position = transform.position;
+		}
+		var vec = Vector3.zero;
+		vec.x = Input.GetAxis("Horizontal");
+		vec.y = Input.GetAxis("Vertical");
+		if(mRigid != null)
+		{
+			mRigid.AddTorque(0.0f, 0.0f, -vec.x, ForceMode.VelocityChange);
+			//var angle = mRigid.rotation.eulerAngles.z - vec.x;
+			//var q = Quaternion.Euler(0.0f, 0.0f, angle);
+			//mRigid.MoveRotation(q);
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			transform.localScale += Vector3.one;
+		}
+	}
+	void OnCollisionEnter(Collision inColl)
+	{
+		if(inColl.gameObject.tag != "Mag")
+		{
+			return;
+		}
+		inColl.transform.SetParent(transform);
+	}
+}
